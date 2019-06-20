0657acd8185796b4bdaf9708e30ee5dc3316f0bb
issamemohtadi <47484433+issamemohtadi@users.noreply.github.com>
2019-06-20 05:07:14 +0100

    Update activate.js

--- activate.js
+++ activate.js
@@ -1,7 +1,44 @@
-<script type="text/javascript">
-//<![CDATA[
-$(document).ready(function() {
-var idBlog= "4322989602444869935"; // معرف المدونة التي وضعت عليها لوحة التفعيل
-$(function(){"use strict";$.ajax({dataType:"json",url:"https://www.blogger.com/feeds/"+idBlog+"/posts/default?alt=json-in-script",method:"GET",dataType:"jsonp",success:function(e){var t;for(t=0;t<e.feed.entry.length;t+=1){var n=$(e.feed.entry[t].content.$t);if(0===t&&!$("body").hasClass("error_page")){for(var o=n.find("li"),d=[],a=0;a<o.length;a+=1)d.push($(o[a]).text());var r,i=window.location.hostname.toLowerCase(),f=window.location.href.toLowerCase(),s=d.length-1;for(r=0;r<d.length;r+=1){if(-1!=i.indexOf(d[r])){var l=$(e.feed.entry[t].content.$t).find("script"),p=$(e.feed.entry[t].content.$t).find("style");$("head").append(p),$("head").append(l);break}r==s&&-1==f.indexOf("post-preview")&&-1==f.indexOf("www.blogger")&&-1==f.indexOf("b/layout-preview")&&-1==f.indexOf("b/preview")&&-1==f.indexOf("translate.google")&&-1==f.indexOf("webcache.googleusercontent")&&-1==f.indexOf("template-editor")&&$("html").html(n.find(".redirect").html())}}if(1===t){p=$(e.feed.entry[t].content.$t).find("style");$("head").append(p)}}}})});});
+
+<script type='text/javascript'>
+ //<![CDATA[
+var _0x9e16 = [ "#sharelink", "#downloadlink", "#copylinkbtog", "keyup paste", "val", "/dl.dropboxusercontent.com/s/&id=$1", "replace", "disabled", "removeAttr", "", "attr", "on", "click", "select", "success", "    ", "تم نسخ الرابط التالي الى الحافظة:", "text", "_blank", "top", "center", "notify", "clearSelection" ];
+
+var _0xe317 = [ _0x9e16[0], _0x9e16[1], _0x9e16[2], _0x9e16[3], _0x9e16[4], _0x9e16[5], _0x9e16[6], _0x9e16[7], _0x9e16[8], _0x9e16[9], _0x9e16[10], _0x9e16[11], _0x9e16[12], _0x9e16[13], _0x9e16[14], _0x9e16[15], _0x9e16[16], _0x9e16[17], _0x9e16[18], _0x9e16[19], _0x9e16[20], _0x9e16[21], _0x9e16[22] ];
+
+!function(a) {
+    a(function() {
+        var b = a(_0xe317[0]), c = a(_0xe317[1]), d = a(_0xe317[2]), e;
+        b[_0xe317[11]](_0xe317[3], function() {
+            var a = b[_0xe317[4]](), e = a[_0xe317[6]](/\/file\/d\/(.+)\/(.+)/, _0xe317[5]);
+            if (e !== a) {
+                c[_0xe317[4]](e);
+                d[_0xe317[8]](_0xe317[7]);
+            } else {
+                c[_0xe317[4]](_0xe317[9]);
+                d[_0xe317[10]](_0xe317[7], _0xe317[7]);
+            }
+        });
+        c[_0xe317[11]](_0xe317[12], function() {
+            c[_0xe317[13]]();
+        });
+        e = new Clipboard(_0xe317[2]);
+        e[_0xe317[11]](_0xe317[14], function(b) {
+            a[_0xe317[21]]({
+                icon: _0xe317[15],
+                title: _0xe317[16],
+                message: b[_0xe317[17]],
+                url: b[_0xe317[17]],
+                target: _0xe317[18]
+            }, {
+                type: _0xe317[14],
+                placement: {
+                    from: _0xe317[19],
+                    align: _0xe317[20]
+                }
+            });
+            b[_0xe317[22]]();
+        });
+    });
+}(jQuery);
 //]]>
 </script>
